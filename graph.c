52f487f7af46b6ba4e9baef4e9038f15299f0203
awmc000 <mccolmalex@gmail.com>
2023-12-18 22:23:48 -0800

    initial

---
+++ graph.c
@@ -0,0 +1,98 @@
+/**
+ * Graph representation designed for use
+ * in a world map geography puzzle game.
+ * 
+ * December 18th, 2023
+ * 
+ * Alex McColm
+ *
+ **/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "graph.h"
+
+struct Vertex * construct_vertex(int i, const char * s)
+{
+	struct Vertex * new_vertex = calloc(1, sizeof(struct Vertex));
+
+	new_vertex->i_data = i;
+	new_vertex->s_data = strdup(s);
+	new_vertex->n_neighbours = 0;
+	new_vertex->neighbours = calloc(1, sizeof(struct Vertex *));
+
+	return new_vertex;
+}
+
+int free_vertex(struct Vertex * vert)
+{
+	free(vert->s_data);
+	
+	if (vert->neighbours != NULL)
+		free(vert->neighbours);
+	
+	free(vert);
+	
+	return 1;
+}
+
+int add_vertex(struct Vertex * a, struct Vertex * b)
+{
+	a->neighbours[a->n_neighbours] = b;
+	b->neighbours[b->n_neighbours] = a;
+
+	a->n_neighbours++;
+	b->n_neighbours++;
+
+	a->neighbours = realloc(a->neighbours, a->n_neighbours);
+	b->neighbours = realloc(b->neighbours, b->n_neighbours);
+
+	return 1;
+}
+
+int are_adjacent(struct Vertex * a, struct Vertex * b)
+{
+	int found = 0;
+
+	// check a for b
+	for (int i = 0; i < a->n_neighbours; i++)
+	{
+		if (a->neighbours[i] == b)
+			found = 1;
+	}
+
+
+	// check b for a (redundant)
+	for (int i = 0; i < b->n_neighbours; i++)
+	{
+		if (b->neighbours[i] == a)
+			found = 1;
+	}
+
+	return found;
+}
+
+void print_neighbours(struct Vertex * a)
+{
+	printf("Neighbours of [%d, %s]: ", a->i_data, a->s_data);
+	for (int i = 0 ; i < a->n_neighbours; i++)
+	{
+		struct Vertex * curr = a->neighbours[i];
+		printf("[%d, %s], ", curr->i_data, curr->s_data);
+	}
+	printf("\n");
+}
+
+// driver code
+int main(void)
+{
+	struct Vertex * a = construct_vertex(1, "a");
+	struct Vertex * b = construct_vertex(2, "b");
+	struct Vertex * c = construct_vertex(3, "b");
+	add_vertex(a, b);
+	add_vertex(a, c);
+	printf("%d, %d\n", are_adjacent(a, b), are_adjacent(a, c));
+	print_neighbours(a);
+	return 0;
+}
---
+++ graph.h
@@ -0,0 +1,68 @@
+/**
+ * Graph representation designed for use
+ * in a world map geography puzzle game.
+ * 
+ * December 18th, 2023
+ * 
+ * Alex McColm
+ *
+ **/
+
+struct Vertex {
+	int i_data; 					// integer data, country code
+	char * s_data; 					// string data, country name
+	int n_neighbours;				// number of neighbours
+	struct Vertex ** neighbours;	// array of neighbours
+};
+
+struct Path {
+	int length;
+	struct Vertex * vertices;
+};
+
+/**
+ * @brief Constructs a vertex to return.
+ * @param i integer data, country code
+ * @param s string data, country name
+ * @return Pointer to vertex allocated on the heap.
+ **/
+struct Vertex * construct_vertex(int i, const char * s);
+
+/**
+ * @brief Frees a vertices' members and then itself.
+ * @param vert Pointer to an existing vertex created with 
+ * construct_vertex.
+ * @return 1 on success.
+ **/
+int free_vertex(struct Vertex * vert);
+
+/**
+ * @brief Creates an edge connecting vertices a and b.
+ * @param a Pointer to a vertex.
+ * @param b Pointer to a vertex.
+ * @return 1 on success.
+ **/
+int add_vertex(struct Vertex * a, struct Vertex * b);
+
+/**
+ * @brief Returns if a is adjacent to b.
+ * @param a Pointer to a vertex.
+ * @param b Pointer to a vertex.
+ * @return 1 if a ~ b, 0 otherwise.
+ **/
+int are_adjacent(struct Vertex * a, struct Vertex * b);
+
+/**
+ * @brief Given a vertex, prints its neighbours to stdout.
+ **/
+void print_neighbours(struct Vertex * a);
+
+/**
+ * @brief Uses breadth-first search to find a path between
+ * vertices a and b.
+ * @param a Pointer to a vertex.
+ * @param b Pointer to a vertex.
+ * @return Pointer to a Path struct if a path was found, NULL 
+ * otherwise.
+ **/
+struct Path * find_path(struct Vertex * a, struct Vertex * b);
