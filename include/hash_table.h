d7f70eafd89a57fd7791e0c15b96b44900d02049
awmc000 <mccolmalex@gmail.com>
2023-12-19 17:40:32 -0800

    formatting into doc comments

--- include/hash_table.h
+++ include/hash_table.h
@@ -25,8 +25,8 @@ struct hash_table {
 };
 
 /**
- * @return a pointer to a newly allocated hashtable or NULL if allocation 
- * failed.
+ * @return a pointer to a newly allocated hashtable or NULL if 
+ * allocation failed.
  **/
 struct hash_table *hashtable_create(size_t initial_size);
 
@@ -37,30 +37,30 @@ struct hash_table *hashtable_create(size_t initial_size);
 int hashtable_insert(struct hash_table *ht, char * s);
 
 /**
- * Removes the string `s` from the hash_table `ht`. Returns 1 if found and 
- * removed and 0 if not found.
+ * @brief Removes the string `s` from the hash_table `ht`. 
+ * @return 1 if found and removed and 0 if not found.
  **/
 int hashtable_remove(struct hash_table *ht, char * s);
 
 /**
- * Removes the string `s` from the hash_table `ht`. Returns 1 if found and 0
- * if not found.
+ * @brief Removes the string `s` from the hash_table `ht`. 
+ * @return 1 if found and 0 if not found.
  **/
 int hashtable_contains(struct hash_table *ht, char * search_key);
 
 /**
- * Resize the hash table to the specified size.
+ * @brief Resize the hash table to the specified size.
  * Uses the C library function `realloc()`.
  **/
 struct hash_table *hashtable_resize(struct hash_table *ht, size_t size);
 
 /**
- * Prints the contents of the hash table to the given file pointer.
+ * @brief Prints the contents of the hash table to the given file pointer.
  **/
 void hashtable_print_contents(struct hash_table *ht, FILE *fp);
 
 /**
- * Frees the contents of a hash table.
+ * @brief Frees the contents of a hash table.
  **/
 void hashtable_delete(struct hash_table *ht);
 
