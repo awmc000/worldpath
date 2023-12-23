ee133b4672bbeac5785a8eb294c6c353db5a94c2
awmc000 <mccolmalex@gmail.com>
2023-12-22 19:32:16 -0800

    include debug symbols in test driver compilation

--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
-gcc test/test.c src/graph.c src/queue.c src/hash_table.c -o testdriver -lcheck -lm -lsubunit
+gcc -g test/test.c src/graph.c src/queue.c src/hash_table.c -o testdriver -lcheck -lm -lsubunit
 ./testdriver
-rm testdriver
