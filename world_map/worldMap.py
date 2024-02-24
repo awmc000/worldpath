102b780e3542fdac215149090b9fc6ab1d84967e
Alex McColm <mccolmalex@gmail.com>
2024-02-23 16:57:25 -0800

    Create and free string variables of country names and alpha2 codes to reduce mem leaks

--- world_map/worldMap.py
+++ world_map/worldMap.py
@@ -5,13 +5,20 @@ import csv
 from signal import signal, SIGPIPE, SIG_DFL  
 signal(SIGPIPE,SIG_DFL) 
 
+nameStrings = []
+alpha2Strings = []
 alpha2ToNameInserts = []
 nameToAlpha2Inserts = []
 vertices = []
 pointers = []
 alpha2ToNumeric = []
 edges = []
+nameFrees = []
+alpha2Frees = []
 
+# TODO: Generate string variables made with strdup and deleted with free for full names and 2-letter codes!
+# Form: ALPHA2_name, ALPHA2_alpha2
+# Eg.: CA_name = strdup("Canada"); CA_alpha2 = strdup("CA");
 
 # For each country C...
 with open('clean-countries.csv') as csvFile:
@@ -23,9 +30,23 @@ with open('clean-countries.csv') as csvFile:
 		countryName = row[0]
 		countryAlpha2 = row[1]
 		countryCode = str(int(row[2]))
+		# Add a string with the country's name
+		nameString = f'char * {countryAlpha2}_name = strdup("{countryName}");'
+		nameStrings.append(nameString)
+
+		nameFree = f'free({countryAlpha2}_name);'
+		nameFrees.append(nameFree)
+
+		# Add a string with the country's alpha2
+		alpha2String = f'char * {countryAlpha2}_alpha2 = strdup("{countryAlpha2}");'
+		alpha2Strings.append(alpha2String)
+
+		alpha2Free = f'free({countryAlpha2}_alpha2);'
+		alpha2Frees.append(alpha2Free)
+
 		# Add the country's name and ISO3 code to the code->name mapping
-		alpha2ToNameInserts.append(f'dictionary_insert(alpha2_to_name, strdup("{countryAlpha2}"), strdup("{countryName}"));')
-		nameToAlpha2Inserts.append(f'dictionary_insert(name_to_alpha2, strdup("{countryName}"), strdup("{countryAlpha2}"));')
+		alpha2ToNameInserts.append(f'dictionary_insert(alpha2_to_name, {countryAlpha2}_alpha2, {countryAlpha2}_name);')
+		nameToAlpha2Inserts.append(f'dictionary_insert(name_to_alpha2, {countryAlpha2}_name, {countryAlpha2}_alpha2));')
 		# Add the country as a vertex
 		vertices.append(f'struct Vertex * country_{countryAlpha2} = construct_vertex({countryCode}, "{countryAlpha2}");')
 		pointers.append(f'countryVertices[{countryCode}] = country_{countryAlpha2};')
@@ -42,11 +63,14 @@ with open('clean-borders.csv') as csvFile:
 		if len(secondAlpha2) > 1:
 			edges.append(f'add_edge(country_{firstAlpha2}, country_{secondAlpha2});')
 
-
+print(*nameStrings, sep = '\n')
+print(*alpha2Strings, sep = '\n')
 print(*alpha2ToNameInserts, sep='\n')
 print(*nameToAlpha2Inserts, sep='\n')
 print(*vertices, sep='\n')
 print(*pointers, sep='\n')
 print(*alpha2ToNumeric, sep='\n')
 print(*edges, sep='\n')
+print(*nameFrees, sep='\n')
+print(*alpha2Frees, sep='\n')
 
