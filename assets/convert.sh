8c20b27f2531039082b1747499bfebd1a9515aac
Alex McColm <mccolmalex@gmail.com>
2024-05-29 20:40:49 -0700

    Get rid of unused assets and their commands in the script; convert font for later use

--- assets/convert.sh
+++ assets/convert.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-grit ball.png -ftb -fh! -gTFF00FF -gt -gB8 -m!
+# Sprites
 grit dot.png -ftb -fh! -gTFF00FF -gt -gB8 -m!
-grit character.png -ftb -fh! -gTFF00FF -gt -gB8 -m!
 
 for file in *.bin; do
     mv -- "$file" "${file%.bin}"
@@ -10,12 +9,26 @@ done
 
 mv *.pal *.img ../nitrofiles/sprite
 
+# Backgrounds
 grit nfl.png -ftb -fh! -gTFF00FF -gt -gB8 -mR8 -mLs
-grit bg2.png -ftb -fh! -gTFF00FF -gt -gB8 -mR8 -mLs
-grit bg3.png -ftb -fh! -gTFF00FF -gt -gB8 -mR8 -mLs
 
 for file in *.bin; do
     mv -- "$file" "${file%.bin}"
 done
 
 mv *.pal *.img *.map ../nitrofiles/bg
+
+# Fonts
+grit font16.png -ftb -fh! -gTFF00FF -gt -gB8 -m!
+
+grit font16.png -ftb -fh! -gTFF00FF -gt -gB8 -m!
+
+for file in *.bin; do
+    mv -- "$file" "${file%.bin}"
+done
+
+for file in *.img; do
+    mv -- "$file" "${file%.img}".fnt
+done
+
+mv *.pal *.fnt ../nitrofiles/fnt
